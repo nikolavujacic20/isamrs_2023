b7e89861839b6948e04b41077ae5294ae4518834
slobodan-zelic <slobodan.zelic@energysage.com>
2023-05-20 17:14:15 +0200

    Backend base

---
+++ isamrs_back/src/main/java/com/isamrs/isamrs_projekat/IsamrsProjekatApplication.java
@@ -0,0 +1,13 @@
+package com.isamrs.isamrs_projekat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsamrsProjekatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsamrsProjekatApplication.class, args);
+	}
+
+}
---
+++ isamrs_back/src/test/java/com/isamrs/isamrs_projekat/IsamrsProjekatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isamrs.isamrs_projekat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsamrsProjekatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
