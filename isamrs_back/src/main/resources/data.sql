a2f227023c1cd03a804504d281be36a2a2f2dffd
slobodan-zelic <slobodan.zelic@energysage.com>
2023-05-21 22:14:36 +0200

    Fill database with users

---
+++ isamrs_back/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into authority (role) values ('ROLE_ADMIN');
+insert into authority (role) values ('ROLE_REGISTERED_USER');
+insert into authority (role) values ('ROLE_INSTRUCTOR');
+
+insert into users (type, email, password, is_active) values ('admin','admin@gmail.com','$2a$10$RVzuprKddsjdq6P8QWmqF.sCj2uYPIUlbFVB.b7tJ9RdFNOOBNoXO' ,true);
+insert into users (type, email, password, is_active) values ('registered_user','user@gmail.com','$2a$10$RVzuprKddsjdq6P8QWmqF.sCj2uYPIUlbFVB.b7tJ9RdFNOOBNoXO' ,true);
+
+insert into users_authority (user_id, authority_id) values (1, 1);
+insert into users_authority (user_id, authority_id) values (2, 2);
